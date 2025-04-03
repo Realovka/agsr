ab008552c796dd35904577d232ce63c75d664bd4
Anastasiya Zvyaginceva <anastasiya.zvyaginceva@digitalchief.ru>
2025-04-03 13:49:18 +0300

    create a new project

---
+++ src/main/java/by/agsr/AgsrApplication.java
@@ -0,0 +1,13 @@
+package by.agsr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgsrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgsrApplication.class, args);
+	}
+
+}
